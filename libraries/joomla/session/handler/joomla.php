4b8a9b1ccc0e35f58cf8336016c6cd668b78e6c1
Matias Aguirre <maguirre@matware.com.ar>
2018-05-22 11:17:36 -0300

    Disable warning

--- libraries/joomla/session/handler/joomla.php
+++ libraries/joomla/session/handler/joomla.php
@@ -45,7 +45,7 @@ class JSessionHandlerJoomla extends JSessionHandlerNative
 		if (!headers_sent())
 		{
 			// Disable transparent sid support
-			ini_set('session.use_trans_sid', '0');
+			@ini_set('session.use_trans_sid', '0');
 
 			// Only allow the session ID to come from cookies and nothing else.
 			if ((int) ini_get('session.use_cookies') !== 1)
